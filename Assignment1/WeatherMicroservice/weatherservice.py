ca1c4816d39fd58c9500444ee7de74b19e1cf1ff
Alami64 <109752794+Alami64@users.noreply.github.com>
2023-02-09 18:40:28 -0800

    Update weatherservice.py

--- Assignment1/WeatherMicroservice/weatherservice.py
+++ Assignment1/WeatherMicroservice/weatherservice.py
@@ -16,7 +16,7 @@ def root():
 
 @app.get("/weather/{cityname}")
 def get_weather() -> str:
-    zipcode = requests.get("http://127.0.0.1:8001/zipcode/san-francisco").text
+    zipcode = requests.get("http://zipode:8001/zipcode/san-francisco").text
     zipcode_list = [int(i) for i in zipcode if i.isdigit()]
     zipcode_int = int("".join(map(str, zipcode_list)))  
     weather_response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?zip={zipcode_int}&appid={config.api_key}")
